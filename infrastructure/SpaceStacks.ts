8bf21e96064c0ef8fc0c18ec183a52d023133164
Alex <alex.dermenji@space01.co.uk>
2023-01-17 09:23:44 +0000

    initial commit

---
+++ infrastructure/SpaceStacks.ts
@@ -0,0 +1,22 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+import { RestApi } from "aws-cdk-lib/aws-apigateway";
+
+export class SpaceStack extends Stack {
+  private api = new RestApi(this, "SpaceApi");
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const helloLambda = new LambdaFunction(this, "HelloLambda", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
+      handler: "hello.main",
+    });
+  }
+}
