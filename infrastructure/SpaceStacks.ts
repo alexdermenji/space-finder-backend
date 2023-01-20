bfb2933e2eacb550045c4376e48291baef577512
Alex <alex.dermenji@space01.co.uk>
2023-01-20 07:57:16 +0000

    lamda gateApi integration

--- infrastructure/SpaceStacks.ts
+++ infrastructure/SpaceStacks.ts
@@ -6,7 +6,7 @@ import {
   Runtime,
 } from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
-import { RestApi } from "aws-cdk-lib/aws-apigateway";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 
 export class SpaceStack extends Stack {
   private api = new RestApi(this, "SpaceApi");
@@ -14,9 +14,14 @@ export class SpaceStack extends Stack {
     super(scope, id, props);
 
     const helloLambda = new LambdaFunction(this, "HelloLambda", {
-      runtime: Runtime.NODEJS_14_X,
+      runtime: Runtime.NODEJS_16_X,
       code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
       handler: "hello.main",
     });
+
+    //HelloLambda integration with API Gateway
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource("hello");
+    helloLambdaResource.addMethod("GET", helloLambdaIntegration);
   }
 }
